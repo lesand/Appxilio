2eb6143b9fdcce2a863cd47c6cf26e427329f09b
lesand <leonelsd7@gmail.com>
2014-03-29 14:37:21 -0600

    adding changes

--- gen/com/example/appxilio/R.java
+++ gen/com/example/appxilio/R.java
@@ -981,9 +981,20 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int ambulance=0x7f020057;
+        public static final int background=0x7f020058;
+        public static final int background_gradient=0x7f020059;
+        public static final int firefighter=0x7f02005a;
+        public static final int help=0x7f02005b;
+        public static final int history=0x7f02005c;
+        public static final int ic_launcher=0x7f02005d;
+        public static final int police=0x7f02005e;
     }
     public static final class id {
+        public static final int ImageAmbulance=0x7f05003f;
+        public static final int ImageFireFighter=0x7f050040;
+        public static final int ImageHistory=0x7f05003e;
+        public static final int ImageStatus=0x7f050042;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -995,7 +1006,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1015,6 +1026,8 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imagePolice=0x7f050041;
+        public static final int imageView1=0x7f05003d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1168,7 +1181,6 @@ containing a value of this type.
         AppBaseTheme from BOTH res/values/styles.xml and
         res/values-v11/styles.xml on API 14+ devices.
     
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f0b0083;
         /**  Application theme. 
